49cc52894bea90d9d2fd98dac9676b2295467310
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-06-23 17:39:31 +0700

    Update installspd.sh

--- installspd.sh
+++ installspd.sh
@@ -4,8 +4,8 @@
 {
 echo -e "Mendownload Binaries"
 sleep 3
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/spd/main/speedtest" -O /usr/bin/speedtest && chmod +x /usr/bin/speedtest
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/spd/main/spd" -O /sbin/spd && chmod +x /sbin/spd
+wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/spd/main/speedtest" -O /usr/bin/speedtest && chmod +x /usr/bin/speedtest
+wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/spd/main/spd" -O /sbin/spd && chmod +x /sbin/spd
 clear
 echo -e "Installasi speedtest Selesai"
 sleep 4
